c561daf5908cc7f6adb6f18c9f32db15d12b805b
Brian Paintsil <brnpaintsil@gmail.com>
2019-02-07 15:08:35 +0000

    cleaning up urls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+const $ = window.$;
 class App extends Component {
   render() {
     return (
       <div className="size1 bg0 where1-parent">
-        <div className="flex-c-m bg-img1 size2 where1 overlay1 where2 respon2" style={{backgroundImage: '%PUBLIC_URL%/images/bg01.jpg'}}>
+        <div className="flex-c-m bg-img1 size2 where1 overlay1 where2 respon2" style={{backgroundImage: `url(images/selena.jpg)`}}>
           <div className="wsize2 flex-w flex-c-m cd100 js-tilt">
             <div className="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
               <span className="l2-txt1 p-b-9 days">35</span>
@@ -31,14 +31,15 @@ class App extends Component {
 
         <div className="size3 flex-col-sb flex-w p-l-75 p-r-75 p-t-45 p-b-45 respon1">
           <div className="wrap-pic1">
-            <img src="%PUBLIC_URL%/images/icons/logo.png" alt="LOGO"/>
+            <p className="">Eve's Hair Signature</p>
+            {/**<img src="images/icons/logo.png" alt="LOGO"/>**/}
           </div>
           <div className="p-t-50 p-b-60">
             <p className="m1-txt1 p-b-36">
               Our website is <span className="m1-txt2">Coming Soon</span>, follow us for update now!
             </p>
 
-            <form className="contact100-form validate-form">
+            {/**<form className="contact100-form validate-form">
               <div className="wrap-input100 m-b-10 validate-input" data-validate="Name is required">
                 <input className="s2-txt1 placeholder0 input100" type="text" name="name" placeholder="Your Name"/>
                 <span className="focus-input100"></span>
@@ -56,25 +57,43 @@ class App extends Component {
 
             <p className="s2-txt3 p-t-18">
               And don’t worry, we hate spam too! You can unsubcribe at anytime.
-            </p>
+            </p>**/}
           </div>
 
           <div className="flex-w">
-            <a href="index.html#" className="flex-c-m size5 bg3 how1 trans-04 m-r-5">
+            <a href="https://www.facebook.com/EveTheHairstylist/" target="_blank" rel="noopener noreferrer" className="flex-c-m size5 bg3 how1 trans-04 m-r-5">
               <i className="fa fa-facebook"></i>
             </a>
-            <a href="index.html#" className="flex-c-m size5 bg4 how1 trans-04 m-r-5">
-              <i className="fa fa-twitter"></i>
+            <a href="https://www.instagram.com/eve_thehairstylist/" target="_blank" rel="noopener noreferrer" className="flex-c-m size5 bg4 how1 trans-04 m-r-5">
+              <i className="fa fa-instagram"></i>
             </a>
-            <a href="index.html#" className="flex-c-m size5 bg5 how1 trans-04 m-r-5">
+            {/** <a href="index.html#" className="flex-c-m size5 bg5 how1 trans-04 m-r-5">
               <i className="fa fa-youtube-play"></i>
-            </a>
+            </a> **/}
           </div>
         </div>
       </div>
-
-
     );
+  }
+
+  componentDidMount() {
+    $('.cd100').countdown100({
+      /*Set Endtime here*/
+      /*Endtime must be > current time*/
+      endtimeYear: 0,
+      endtimeMonth: 0,
+      endtimeDate: 30,
+      endtimeHours: 18,
+      endtimeMinutes: 0,
+      endtimeSeconds: 0,
+      timeZone: "" 
+      // ex:  timeZone: "America/New_York"
+      //go to " http://momentjs.com/timezone/ " to get timezone
+    });
+
+    $('.js-tilt').tilt({
+      scale: 1.1
+    })
   }
 }
 
