e8d4791cfc02918dda44b0406ac9ac7b5a5511c9
Brian Paintsil <brnpaintsil@gmail.com>
2019-04-20 08:53:54 +0000

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
 import React, { Component } from 'react';
-import Header from '../src/components/header';
-import MobileMenu from './components/mobile-menu';
-import Slider from './components/slider';
-import Salon from './components/salon';
-import Services from './components/services';
-import Contact from './components/contact';
-import Appointment from './components/appointment';
-import Ourteam from './components/team';
-const $ = window.$;
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Main from './main';
+
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <MobileMenu />
-        <div id="page">
-          <Header />
-          <Slider />
-          <Ourteam />
-          {/*<Salon />*/}
-          <Services />
-          <Appointment />
-          <Contact />
-          <a href="index.html#" className="scrollup">Top</a>
-        </div> 
-      </React.Fragment>
+      <Router>
+        <Route exact path="/" render={props => <Main {...props}/> } />
+      </Router>
     )
-  }
-
-  componentDidMount() {
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/jquery.themepunch.tools.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/jquery.themepunch.revolution.min.js');
-
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revolution.extension.layeranimation.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revolution.extension.migration.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revolution.extension.navigation.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revolution.extension.parallax.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revolution.extension.slideanims.min.js');
-    // $.getScript(process.env.PUBLIC_URL + 'js/revoluation/revoluationfunction.js');
   }
 }
 
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Header from '../src/components/header';
+import MobileMenu from './components/mobile-menu';
+import Slider from './components/slider';
+// import Salon from './components/salon';
+import Services from './components/services';
+import Contact from './components/contact';
+import Appointment from './components/appointment';
+import Ourteam from './components/team';
+
+class Main extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MobileMenu />
+        <div id="page">
+          <Header />
+          <Slider />
+          <Ourteam />
+          {/*<Salon />*/}
+          <Services />
+          <Appointment />
+          <Contact />
+          <a href="index.html#" className="scrollup">Top</a>
+        </div> 
+      </React.Fragment>
+    )
+  }
+}
+
+export default Main;
